da0cdab323264256cb0f2caf7c7465539b9d9ae8
tgbiztank <tgbiztank@gmail.com>
2022-07-12 00:25:39 +0700

    rename packer to plugins

--- homefolder/.config/nvim/init.lua
+++ homefolder/.config/nvim/init.lua
@@ -1,19 +1,20 @@
 -- plugins
-require "plugins.packer"
--- optional 
-require "options.general"
-require "options.keymaps"
+-- require("plugins.packer")
+require("plugins.plugins")
+-- optional
+require("options.general")
+require("options.keymaps")
 -- config plugins
-require "plugins.configs.autopairs"
-require "plugins.configs.barbar"
-require "plugins.configs.cmp"
-require "plugins.configs.colorizer"
-require "plugins.configs.comment"
-require "plugins.configs.dracula"
-require "plugins.configs.indentline"
-require "plugins.configs.lsp"
-require "plugins.configs.lualine"
-require "plugins.configs.nvim-tree"
-require "plugins.configs.telescope"
-require "plugins.configs.treesitter"
-require "plugins.configs.copilot"
+require("plugins.configs.autopairs")
+require("plugins.configs.barbar")
+require("plugins.configs.cmp")
+require("plugins.configs.colorizer")
+require("plugins.configs.comment")
+require("plugins.configs.dracula")
+require("plugins.configs.indentline")
+require("plugins.configs.lsp")
+require("plugins.configs.lualine")
+require("plugins.configs.nvim-tree")
+require("plugins.configs.telescope")
+require("plugins.configs.treesitter")
+require("plugins.configs.copilot")
