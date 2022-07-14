421cfb7160b7a01ed9bfb2d49366eb35dee96d04
Tạ Tuấn Anh <69012511+tgbiztank@users.noreply.github.com>
2022-07-14 10:18:31 +0700

    Delete home/.config/picom/i3 directory

--- home/.config/picom/i3/script/ibus/toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CHECK_INPUT_METHOD=$(ibus engine)
-
-if [ "$CHECK_INPUT_METHOD" = "Bamboo" ]; then
-  ibus engine "BambooUs"
-else
-  ibus engine "Bamboo"
-fi
--- home/.config/picom/i3/script/pulse/toggle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-killall pulseaudio
-pulseaudio -k
-sleep 1
-pulseaudio --start -D
-pulseaudio-ctl mute
